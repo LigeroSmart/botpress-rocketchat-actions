6da4d4bd48ad4a3065dea5750d56dc1f57e7de3a
richieri <richieri@gmail.com>
2021-07-01 16:28:31 -0300

    Update rocketchat-Transfer.js

--- rocketchat-Transfer.js
+++ rocketchat-Transfer.js
@@ -1,14 +1,16 @@
 function action(bp: typeof sdk, event: sdk.IO.IncomingEvent, args: any, { user, temp, session } = event.state) {
   /** Your code starts below */
 
+  /* You must provid the Botpress App Webhook from Rocketchat */
+  const rocketWebhook = 'http://localhost:3001/api/apps/public/6a8e6b6c-7a25-47c9-94e8-5af5075981ad/incoming'
+
   const axios = require('axios')
 
   /**
    * @title Transfer visitor to departament
    * @category Channel Rocket.Chat
    * @author LigeroSmart https://ligerosmart.com
-   * @param {string} rocketchatHost Rocket.Chat HOST URL
-   * @param {string} departmentId Department ID
+   * @param {string} department Department Name
    * @param {string} [transferText=] Text to be shown when transfer visitor
    */
   const callApi = async () => {
@@ -23,12 +25,14 @@ function action(bp: typeof sdk, event: sdk.IO.IncomingEvent, args: any, { user, 
     // Send the message to the user (note the array, since you can send multiple payloads in the same reply)
     await bp.events.replyToEvent(event, [message])
     const RoomIdAndVisitorToken = event.target.split(':')
-    const apiURL = `${args.rocketchatHost}/api/v1/livechat/room.transfer`
+
     // Transfer user
-    const { data } = await axios.post(apiURL, {
-      rid: RoomIdAndVisitorToken[0],
-      token: RoomIdAndVisitorToken[1],
-      department: args.departmentId
+    const { data } = await axios.post(rocketWebhook, {
+      sessionId: RoomIdAndVisitorToken[0],
+      action: 'handover',
+      actionData: {
+        targetDepartment: args.department
+      }
     })
 
     // The first element returned is the most recent release
